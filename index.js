58ce7e550971a42a92cd34dc616eedbe6665f3f1
Nafiz <nafiz6@yahoo.com>
2020-04-15 02:08:55 +0600

    test

--- index.js
+++ index.js
@@ -5,31 +5,14 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 
-    exec("./a.out inp.c", (error, stdout, stderr) => {
 
-  try{  const contents = fs.readFileSync('code.asm', 'UTF-8');
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	const lines = contents.split(/\r?\n/);
-	lines.forEach((line) => {
-    res.write(line);
+
+    res.write("HELLOW");
     res.write("\n");	
 
 
-})
-
-	res.end();
-} catch(err){
-console.error(err);
-}	
 
 
-});
+
+
 }).listen(8080);
