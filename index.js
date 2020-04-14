47dc80648d97780fa90f353994ae46750d2d6c0a
Nafiz <nafiz6@yahoo.com>
2020-04-15 01:52:19 +0600

    exec

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const { exec } = require("child_process");
 var http = require('http');
 
 http.createServer(function (req, res) {
-  try{  const contents = fs.readFileSync('code.asm', 'UTF-8');
+
     exec("./a.out inp.c", (error, stdout, stderr) => {
+
+  try{  const contents = fs.readFileSync('code.asm', 'UTF-8');
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -14,19 +16,20 @@ http.createServer(function (req, res) {
         console.log(`stderr: ${stderr}`);
         return;
     }
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	const lines = contents.split(/\r?\n/);
+	lines.forEach((line) => {
+    res.write(line);
+    res.write("\n");	
 
 
 })
 
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	const lines = contents.split(/\r?\n/);
-	lines.forEach((line) => {
-	   res.write(line);
-	    res.write("\n");	
-});
 	res.end();
 } catch(err){
 console.error(err);
 }	
 
+
+});
 }).listen(8080);
