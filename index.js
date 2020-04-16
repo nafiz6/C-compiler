510bb74fcf41a1f591d8b1f364790d53ce5a0e4f
Nafiz <nafiz6@yahoo.com>
2020-04-16 21:50:31 +0600

    Post rest api

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-var express = require("express");
-var app = express();
-var fs = require("fs");
+const express = require("express"),
+	bodyParser = require('body-parser'),
+	app = express().use(bodyParser.json()),
+	fs = require("fs");
 
 const { exec } = require("child_process");
-var http = require('http');
-
-app.get('/', (req,res) =>{
 
 
-
-    exec("./a.out inp.c", (error, stdout, stderr) => {
-
-  try{  const contents = fs.readFileSync('code.asm', 'UTF-8');
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-
-	const lines = contents.split(/\r?\n/);
+app.listen(process.env.PORT || 8000, () => console.log('webhook is listening'));
 
 
 
+app.get('/last-file', (req,res) =>{
+	exec("./a.out inp.c", (error, stdout, stderr) => {
+
+	try{
+		const contents = fs.readFileSync('code.asm', 'UTF-8');
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+
+		const lines = contents.split(/\r?\n/); 
 
 
+		res.status(200).send(contents);
 
-
-
-    res.status(200).send(contents);
-
-} catch(err){
-console.log(err)
-}});
+	} catch(err){
+		console.log(err)
+	}});
 });
 
 
 
+app.post('/', (req,res) =>{
+	let body = req.body;
+	console.log(req.body);
+	fs.writeFile('inp.c', body.code ,function (err) {
+		if (err) throw err;
+		console.log('Saved!');
+	});
 
-app.listen(process.env.PORT || 8000, () => console.log('webhook is listening'));
+	exec("./a.out inp.c", (error, stdout, stderr) => {
+
+	try{
+		const contents = fs.readFileSync('code.asm', 'UTF-8');
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+
+		const lines = contents.split(/\r?\n/);
+		let sendVal={};
+		sendVal.code=contents;
+		res.status(200).send(sendVal);
+
+	} catch(err){
+		console.log(err)
+	}});
+});
+
+
 
 
